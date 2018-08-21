91f137d6306d3bf583ccc9a74c891243d2bc4374
Kurek <KUREKM@uk.softlab.net>
2018-08-21 12:03:24 +0100

    app components structure added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import PlanInfo from './components/plan_info.js';
+// import PlanCategory from './components/plan_category.js';
+// import PlanTitle from './components/plan_title.js';
+// import PlanGoals from './components/plan_goals.js';
 
 class App extends Component {
   render() {
     return (
       <div>
         <PlanInfo />
+        {/* <PlanCategory />
+        <PlanTitle />
+        <PlanGoals /> */}
       </div>
     );
   }
