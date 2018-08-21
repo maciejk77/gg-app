1206718735f9e161c4974db77cadd4147c666476
Jonas <JONAST@uk.softlab.net>
2018-08-21 16:56:07 +0100

    plan goal component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,65 @@
 import React, { Component } from 'react';
-import PlanInfo from './components/plan_info.js';
-import PlanCategory from './components/plan_category.js';
-import PlanTitle from './components/plan_title.js';
-// import PlanGoals from './components/plan_goals.js';
+//import PlanInfo from './components/plan_info.js';
+// import PlanCategory from './components/plan_category.js';
+// import PlanTitle from './components/plan_title.js';
+import PlanGoals from './components/plan_goals.js';
 
 class App extends Component {
+
+  state = {
+    goal : 0,
+    saved: 0,
+    date: {
+      day: new Date().getDate(),
+      month: new Date().getMonth(),
+      year: new Date().getFullYear()
+    },
+    save_daily: 0,
+    plan_title: 'test title'
+  }
+
+  onGoalChange = (e) => {
+    this.setState({
+      ...this.state,
+      goal : e.target.value
+    },this.calculateDaily())
+  }
+
+  onSavedChange = (e) => {
+    this.setState({
+      ...this.state,
+      saved : e.target.value
+    },this.calculateDaily())
+  }
+
+  onDateChange = (e) => {
+    let date = this.state.date;
+    const name = e.target.name;
+    const value = e.target.value;
+    date[name] = value;
+    
+    this.setState({
+      ...this.state,
+      date
+    },this.calculateDaily())
+  }
+
+  calculateDaily = () => {
+    
+  }
+
   render() {
     return (
       <div>
-        <PlanInfo />
-        <PlanCategory />
-        <PlanTitle />
-        {/* <PlanGoals /> */}
+        {/*<PlanInfo />
+         <PlanCategory />
+        <PlanTitle />*/}
+        <PlanGoals 
+          state={this.state}
+          changeGoal={this.onGoalChange} 
+          changeSaved={this.onSavedChange} 
+          handleDateChange={this.onDateChange}
+        /> 
       </div>
     );
   }
---
+++ src/components/plan_goals.js
@@ -0,0 +1,96 @@
+import React from "react";
+
+const PlanGoals = ({
+  state: { plan_title, goal, saved, date, save_daily, date: {day, month, year} },
+  changeGoal,
+  changeSaved,
+  handleDateChange
+}) => {
+  const MONTHS = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "Maj",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+
+  const dateNow = new Date();
+  const yearMin = dateNow.getFullYear();
+  const yearMax = yearMin + 20;
+
+  const getYears = (yearMin, yearMax) => {
+    const length = yearMax - yearMin;
+    return Array.from({ length }, (_, i) => {
+      return (
+        <option
+          key={i}
+          value={yearMin + i}
+          selected={year === yearMin + i ? "selected" : false}
+        >
+          {yearMin + i}
+        </option>
+      );
+    });
+  };
+
+  const years = getYears(yearMin, yearMax);
+  const months = MONTHS.map((month_const, i) => (
+    <option
+      key={i}
+      value={i}
+      selected={month === i ? "selected" : false}
+    >
+      {month_const}
+    </option>
+  ))
+  const days = Array.from({ length: 31 }, (_, i) => (
+    <option
+      key={i}
+      value={i + 1}
+      selected={day === i + 1 ? "selected" : false}
+    >
+      {i + 1}
+    </option>
+  ));
+
+  return (
+    <div>
+      Set your goal
+      <form>
+        <div>
+          <input type="text" value={goal} onChange={changeGoal} />
+        </div>
+
+        <div>
+          <input type="text" value={saved} onChange={changeSaved} />
+        </div>
+
+        <div>
+          <select name="day" onChange={handleDateChange}>
+            {days}
+          </select>
+          
+          <select name="month" onChange={handleDateChange}>
+            {months}
+          </select>
+          
+          <select name="year" onChange={handleDateChange}>
+            {years}
+          </select>
+        </div>
+        <div>
+          <input name="save_daily" value={save_daily} disabled />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default PlanGoals;
