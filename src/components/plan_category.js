b48246eb09bbed01efdbf4037f566a203e60e69b
Kurek <KUREKM@uk.softlab.net>
2018-08-21 12:32:01 +0100

    PlanItem component added

--- src/components/plan_category.js
+++ src/components/plan_category.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import data from '../data/data.json';
+import PlanItem from './plan_item.js';
 
 const PlanCategory = () => {
-    return <div> Hello plan catogory</div>
+    return data.map(item => {
+        return <PlanItem
+                key={item.id} 
+                id={item.id}
+                goal={item.goal}
+                source={item.source}
+        /> 
+    });
 } 
 
 export default PlanCategory;
---
+++ src/components/plan_item.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PlanItem = (props) => {
+    return <div>{props.goal}</div>
+}
+
+export default PlanItem;
