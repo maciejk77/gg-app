91a1e2066d966b55a0d218a80eddbf95cf360bc2
Jonas <JONAST@uk.softlab.net>
2018-08-23 16:30:22 +0100

    countdown initial setup

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -1,1 +1,20 @@
-test;
+import React, { Component } from "react";
+import moment from "moment";
+
+class Countdown extends Component {
+  render() {
+    const future = moment("2016-06-06T21:03:55"); //now
+    const start = moment("2016-05-06T20:03:55");
+
+    return (
+      <div>
+        <div>
+          {moment(future - start).format(
+            "M[ month(s) ] D[ day(s)] H[ hour(s)] m[ minute(s)] s[ second(s) ]"
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+export default Countdown;
