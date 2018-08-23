c706b32c0129e90ad74539b94bdee11c1bcba603
Jonas <JONAST@uk.softlab.net>
2018-08-23 11:59:02 +0100

    create countdown component

--- src/components/plan_info.js
+++ src/components/plan_info.js
@@ -1,43 +1,55 @@
-import React, { Component } from 'react';
-import '../styles/plan_info.css';
+import React, { Component } from "react";
+import Countdown from "./countdown";
+import "../styles/plan_info.css";
 
 class PlanInfo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isActive: false
-        }
-        this.handle_click = this.handle_click.bind(this);
-    }
-   
-    handle_click() {
-        this.setState(prevState => ({
-            isActive: !prevState.isActive
-          }));
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: false
+    };
+    this.handle_click = this.handle_click.bind(this);
+  }
 
-    render() {
-        const { goal, saved, date:{day, month, year}, isActive } = this.props;
-        //if(!isActive) { return null };
-        return (
-            <div className="plan__info">
-                <div>Hi NAME</div>  
-                    <div>
-                        <div>Currently in savings: </div>
-                        <div>-------bar here--------</div>
-                        <div>£{saved} of £{goal}</div>
-                        <div>Time left to hit your saving target:</div>
-                        <div>{day} {month} {year} ----------fix month format and value</div>
-                        <div>DATE 11 mths 22 days 09 hours 54 min</div>
-                        { goal   
-                          ? <div>{Math.floor((saved/goal)*100)}%</div>
-                          : <div>0%</div>
-                        } 
-                    </div>
-                <button onClick={this.handle_click}>Adjust my goal</button>
-            </div>
-        )
-    }
+  handle_click() {
+    this.setState(prevState => ({
+      isActive: !prevState.isActive
+    }));
+  }
+
+  render() {
+    const {
+      goal,
+      saved,
+      date: { day, month, year },
+      isActive
+    } = this.props;
+    //if(!isActive) { return null };
+    return (
+      <div className="plan__info">
+        <div>Hi NAME</div>
+        <div>
+          <div>Currently in savings: </div>
+          <div>-------bar here--------</div>
+          <div>
+            £{saved} of £{goal}
+          </div>
+          <div>Time left to hit your saving target:</div>
+          <div>
+            {day} {month} {year} ----------fix month format and value
+          </div>
+          <div>DATE 11 mths 22 days 09 hours 54 min</div>
+          <Countdown />
+          {goal ? (
+            <div>{Math.floor((saved / goal) * 100)}%</div>
+          ) : (
+            <div>0%</div>
+          )}
+        </div>
+        <button onClick={this.handle_click}>Adjust my goal</button>
+      </div>
+    );
+  }
 }
 
 export default PlanInfo;
