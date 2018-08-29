4daf637a67b2589d9f6bf6d4a6df676b0d1f1dee
Sanjeev Baurai <sanjeev.baurai@gmail.com>
2018-08-29 14:25:55 +0100

    highlighting on category items

--- src/components/plan_category.js
+++ src/components/plan_category.js
@@ -9,29 +9,40 @@ class PlanCategory extends Component {
     constructor(props) {
         super(props);
 
-        this.render_list = this.render_list.bind(this);
+        this.state = {
+            selected: 0
+        }       
     }
 
-    render_list() {
-        return this.props.data.map(item => {
-            return (
-                <PlanItem
-                    key={item.id} 
-                    id={item.id}
-                    goal={item.goal}
-                    source={item.source}
-                    getTitle={this.props.getTitle}
-                />
-            ) 
+    handle_click = (e) => {
+        e.preventDefault();
+		// this.props.getTitle(this.props.goal);
+		this.setState({
+            selected: parseInt(e.currentTarget.dataset.id) - 1
         });
-    }    
+    }
+    
     render() {
+        const render_list = this.props.data.map((item,i) => {
+                return (
+                    <PlanItem
+                        key={item.id} 
+                        id={item.id}
+                        goal={item.goal}
+                        source={item.source}
+                        getTitle={this.props.getTitle}
+                        handle_click={(e) => this.handle_click(e)}
+                        selected={i === this.state.selected ? true : false}
+                    />
+                ) 
+            });
+          
         return (
             <div className="plan-category">
                 <StepsBar step={this.props.step}/>
                 <div>What are you saving for?</div>
                 <div className="plan-category__list">
-                    {this.render_list()}
+                    {render_list}
                 </div>
                 <div className="button-group">
                   <Link to="/title" className="button-group__next"> Next </Link>
--- src/components/plan_item.js
+++ src/components/plan_item.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/plan_item.css';
 import propTypes from 'prop-types';
+import cn from 'classnames';
 
-const PlanItem = (props) => {
-  const { goal, source, getTitle } = props;
-    return (
-        <div className="plan-item" onClick={() => getTitle(goal)}>
-            <img className="plan-item__image" src={`/images/${source}.png`} style={{width: "40%"}} alt="" />
-            {goal}
-        </div>
-    )
+class PlanItem extends Component {
+
+	render() {
+		const { goal, source, id, selected } = this.props;
+		let activeClass = (selected ? 'plan-item--active' : 'plan-item--nonactive');
+		return (
+			<div key={id} className={cn("plan-item", activeClass)} data-id={id} onClick={this.props.handle_click}>
+				<img className="plan-item__image" src={`/images/${source}.png`} style={{ width: "40%" }} alt="" />
+				{goal}
+			</div>
+		)
+	}
 }
 
 export default PlanItem;
 
 PlanItem.propTypes = {
-  goal: propTypes.string,
-  source: propTypes.string,
-  getTitle: propTypes.func 
+	goal: propTypes.string,
+	source: propTypes.string,
+	getTitle: propTypes.func,
+	id: propTypes.number
 };
